9c851e3ba2e610ead9e6dd3052708679cb7b39fc
RedHeadphone <khilawala92@gmail.com>
2021-09-30 20:57:05 +0530

    added events

--- Backend/SeedDb.js
+++ Backend/SeedDb.js
@@ -21,7 +21,8 @@ async function seedDB() {
     //         numberOpenings: Math.floor(Math.random() * 10+1),
     //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
     //         perks: 'Certificate',
-    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)]
+    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)],
+    //         category: ['Internship','Research','Recruitment'][Math.floor(Math.random() * 2)]
     //     },
     //     {
     //         faculty: "6155bc052426181b3443d959",
@@ -32,7 +33,8 @@ async function seedDB() {
     //         numberOpenings: Math.floor(Math.random() * 10+1),
     //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
     //         perks: 'Certificate',
-    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)]
+    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)],
+    //         category: ['Internship','Research','Recruitment'][Math.floor(Math.random() * 2)]
     //     },
     //     {
     //         faculty: "6155bc052426181b3443d959",
@@ -43,7 +45,8 @@ async function seedDB() {
     //         numberOpenings: Math.floor(Math.random() * 10+1),
     //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
     //         perks: 'Certificate',
-    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)]
+    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)],
+    //         category: ['Internship','Research','Recruitment'][Math.floor(Math.random() * 2)]
     //     },
     //     {
     //         faculty: "6155bc052426181b3443d959",
@@ -54,7 +57,8 @@ async function seedDB() {
     //         numberOpenings: Math.floor(Math.random() * 10+1),
     //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
     //         perks: 'Certificate',
-    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)]
+    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)],
+    //         category: ['Internship','Research','Recruitment'][Math.floor(Math.random() * 2)]
     //     },
     //     {
     //         faculty: "6155bc052426181b3443d959",
@@ -65,7 +69,8 @@ async function seedDB() {
     //         numberOpenings: Math.floor(Math.random() * 10+1),
     //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
     //         perks: 'Certificate',
-    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)]
+    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)],
+    //         category: ['Internship','Research','Recruitment'][Math.floor(Math.random() * 2)]
     //     },
     //     {
     //         faculty: "6155bc6f2426181b3443d95f",
@@ -76,7 +81,8 @@ async function seedDB() {
     //         numberOpenings: Math.floor(Math.random() * 10+1),
     //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
     //         perks: 'Certificate',
-    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)]
+    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)],
+    //         category: ['Internship','Research','Recruitment'][Math.floor(Math.random() * 2)]
     //     },
     //     {
     //         faculty: "6155bc6f2426181b3443d95f",
@@ -87,7 +93,8 @@ async function seedDB() {
     //         numberOpenings: Math.floor(Math.random() * 10+1),
     //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
     //         perks: 'Certificate',
-    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)]
+    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)],
+    //         category: ['Internship','Research','Recruitment'][Math.floor(Math.random() * 2)]
     //     },
     //     {
     //         faculty: "6155bc6f2426181b3443d95f",
@@ -98,7 +105,8 @@ async function seedDB() {
     //         numberOpenings: Math.floor(Math.random() * 10+1),
     //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
     //         perks: 'Certificate',
-    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)]
+    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)],
+    //         category: ['Internship','Research','Recruitment'][Math.floor(Math.random() * 2)]
     //     },
     //     {
     //         faculty: "6155bc6f2426181b3443d95f",
@@ -109,7 +117,8 @@ async function seedDB() {
     //         numberOpenings: Math.floor(Math.random() * 10+1),
     //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
     //         perks: 'Certificate',
-    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)]
+    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)],
+    //         category: ['Internship','Research','Recruitment'][Math.floor(Math.random() * 2)]
     //     },
     //     {
     //         faculty: "6155bc6f2426181b3443d95f",
@@ -120,16 +129,165 @@ async function seedDB() {
     //         numberOpenings: Math.floor(Math.random() * 10+1),
     //         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
     //         perks: 'Certificate',
-    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)]
+    //         type: ['External','Work from Home'][Math.floor(Math.random() * 2)],
+    //         category: ['Internship','Research','Recruitment'][Math.floor(Math.random() * 2)]
     //     }
     // ];
-    // for (let i = 0; i < 7; i++) {
+    // for (let i = 0; i < 10; i++) {
     //     for (let i = 0; i < 10; i++) {
     //         db.InternshipDetails.create(ints[i])
     //             .then((internship) => console.log(internship))
     //             .catch(err => console.log(err))
     //     }
     // }
+
+
+
+
+
+    events =[
+        {
+            organiser: "6155cf0752ad2031dfaa1523",
+            title: "Lorem ipsum dolor sit amet",
+            venue: "Room number "+randrange(100,599),
+            startTime: "5 PM",
+            endTime: "7 PM",
+            date: addDays(Math.floor(Math.random() * 40)+1),
+            category: ['Competetion', 'Workshop', 'Culture & Entertainment', 'Seminar', 'Webinar', 'Social Work'][Math.floor(Math.random() * 6)],
+            link: "https://www.microsoft.com/en-us/event",
+            photo: "https://randomwordgenerator.com/img/picture-generator/57e5d04a4a52ac14f1dc8460962e33791c3ad6e04e507440762e79d7924ac5_640.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
+            applyBy: new Date(),
+            openToAll: [true,false][Math.floor(Math.random() * 2)]
+        },
+        {
+            organiser: "6155cf0752ad2031dfaa1523",
+            title: "Lorem ipsum dolor sit amet",
+            venue: "Room number "+randrange(100,599),
+            startTime: "5 PM",
+            endTime: "7 PM",
+            date: addDays(Math.floor(Math.random() * 40)+1),
+            category: ['Competetion', 'Workshop', 'Culture & Entertainment', 'Seminar', 'Webinar', 'Social Work'][Math.floor(Math.random() * 6)],
+            link: "https://www.microsoft.com/en-us/event",
+            photo: "https://randomwordgenerator.com/img/picture-generator/54e8d0414851ad14f1dc8460962e33791c3ad6e04e50744172287cd09f48c5_640.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
+            applyBy: new Date(),
+            openToAll: [true,false][Math.floor(Math.random() * 2)]
+        },
+        {
+            organiser: "6155cf0752ad2031dfaa1523",
+            title: "Lorem ipsum dolor sit amet",
+            venue: "Room number "+randrange(100,599),
+            startTime: "5 PM",
+            endTime: "7 PM",
+            date: addDays(Math.floor(Math.random() * 40)+1),
+            category: ['Competetion', 'Workshop', 'Culture & Entertainment', 'Seminar', 'Webinar', 'Social Work'][Math.floor(Math.random() * 6)],
+            link: "https://www.microsoft.com/en-us/event",
+            photo: "https://randomwordgenerator.com/img/picture-generator/54e0d6444e53a414f1dc8460962e33791c3ad6e04e5074417c2d78d39444c4_640.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
+            applyBy: new Date(),
+            openToAll: [true,false][Math.floor(Math.random() * 2)]
+        },
+        {
+            organiser: "6155cf0752ad2031dfaa1523",
+            title: "Lorem ipsum dolor sit amet",
+            venue: "Room number "+randrange(100,599),
+            startTime: "5 PM",
+            endTime: "7 PM",
+            date: addDays(Math.floor(Math.random() * 40)+1),
+            category: ['Competetion', 'Workshop', 'Culture & Entertainment', 'Seminar', 'Webinar', 'Social Work'][Math.floor(Math.random() * 6)],
+            link: "https://www.microsoft.com/en-us/event",
+            photo: "https://randomwordgenerator.com/img/picture-generator/54e6d5464f52ae14f1dc8460962e33791c3ad6e04e50744172297cdc9e48c3_640.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
+            applyBy: new Date(),
+            openToAll: [true,false][Math.floor(Math.random() * 2)]
+        },
+        {
+            organiser: "6155cf0752ad2031dfaa1523",
+            title: "Lorem ipsum dolor sit amet",
+            venue: "Room number "+randrange(100,599),
+            startTime: "5 PM",
+            endTime: "7 PM",
+            date: addDays(Math.floor(Math.random() * 40)+1),
+            category: ['Competetion', 'Workshop', 'Culture & Entertainment', 'Seminar', 'Webinar', 'Social Work'][Math.floor(Math.random() * 6)],
+            link: "https://www.microsoft.com/en-us/event",
+            photo: "https://randomwordgenerator.com/img/picture-generator/54e6d4474f50a814f1dc8460962e33791c3ad6e04e5074417c2c7fd59e48c3_640.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
+            applyBy: new Date(),
+            openToAll: [true,false][Math.floor(Math.random() * 2)]
+        },
+        {
+            organiser: "6155cf0752ad2031dfaa1523",
+            title: "Lorem ipsum dolor sit amet",
+            venue: "Room number "+randrange(100,599),
+            startTime: "5 PM",
+            endTime: "7 PM",
+            date: addDays(Math.floor(Math.random() * 40)+1),
+            category: ['Competetion', 'Workshop', 'Culture & Entertainment', 'Seminar', 'Webinar', 'Social Work'][Math.floor(Math.random() * 6)],
+            link: "https://www.microsoft.com/en-us/event",
+            photo: "https://randomwordgenerator.com/img/picture-generator/53e1d7444d53af14f1dc8460962e33791c3ad6e04e507440742a7ad19f4cc0_640.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
+            applyBy: new Date(),
+            openToAll: [true,false][Math.floor(Math.random() * 2)]
+        },
+        {
+            organiser: "6155cf0752ad2031dfaa1523",
+            title: "Lorem ipsum dolor sit amet",
+            venue: "Room number "+randrange(100,599),
+            startTime: "5 PM",
+            endTime: "7 PM",
+            date: addDays(Math.floor(Math.random() * 40)+1),
+            category: ['Competetion', 'Workshop', 'Culture & Entertainment', 'Seminar', 'Webinar', 'Social Work'][Math.floor(Math.random() * 6)],
+            link: "https://www.microsoft.com/en-us/event",
+            photo: "https://randomwordgenerator.com/img/picture-generator/52e7dc414851af14f1dc8460962e33791c3ad6e04e5077497c2a7cd49349cc_640.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
+            applyBy: new Date(),
+            openToAll: [true,false][Math.floor(Math.random() * 2)]
+        },
+        {
+            organiser: "6155cf0752ad2031dfaa1523",
+            title: "Lorem ipsum dolor sit amet",
+            venue: "Room number "+randrange(100,599),
+            startTime: "5 PM",
+            endTime: "7 PM",
+            date: addDays(Math.floor(Math.random() * 40)+1),
+            category: ['Competetion', 'Workshop', 'Culture & Entertainment', 'Seminar', 'Webinar', 'Social Work'][Math.floor(Math.random() * 6)],
+            link: "https://www.microsoft.com/en-us/event",
+            photo: "https://randomwordgenerator.com/img/picture-generator/gae70ff0c9be5b5f10c08736584b1dcd6308500f6dab9e94e500464a9774c8bbd0a0d6c620b806ad6f7ce814363416e3d_640.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
+            applyBy: new Date(),
+            openToAll: [true,false][Math.floor(Math.random() * 2)]
+        },
+        {
+            organiser: "6155cf0752ad2031dfaa1523",
+            title: "Lorem ipsum dolor sit amet",
+            venue: "Room number "+randrange(100,599),
+            startTime: "5 PM",
+            endTime: "7 PM",
+            date: addDays(Math.floor(Math.random() * 40)+1),
+            category: ['Competetion', 'Workshop', 'Culture & Entertainment', 'Seminar', 'Webinar', 'Social Work'][Math.floor(Math.random() * 6)],
+            link: "https://www.microsoft.com/en-us/event",
+            photo: "https://randomwordgenerator.com/img/picture-generator/54e9d6474257ab14f1dc8460962e33791c3ad6e04e507749712e79d29348c2_640.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus tempore eos quibusdam, tenetur quas nihil asperiores molestiae ad sunt, dolore, minima blanditiis? Error unde sapiente, temporibus sit eius neque.",
+            applyBy: new Date(),
+            openToAll: [true,false][Math.floor(Math.random() * 2)]
+        },
+    ]
+    // for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 3; i++) {
+        db.Event.create(events[i])
+            .then((internship) => console.log(internship))
+            .catch(err => console.log(err))
+    }
+    // }
+
+
+
+
+
+
+
+
     // data = {};
     // for (let i = 0; i < 5; i++) {
     //     data = {
--- Frontend/src/containers/Events/Event.js
+++ Frontend/src/containers/Events/Event.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Event(props) {
+    console.log(props)
     return (
         <div className="card" onClick={()=>props.history.push(`/event/${props._id}`)}>
             <div className="image">
