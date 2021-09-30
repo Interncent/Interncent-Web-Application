81d5ba336d0e0dcab4c5d0bf60a49e09ed8b77db
Vedant13-ux <vedant.nagani@somaiya.edu>
2021-09-30 20:25:56 +0530

    event fix

--- Frontend/src/compenents/IntroductionPage.js
+++ Frontend/src/compenents/IntroductionPage.js
@@ -8,14 +8,14 @@ import Testimonials from '../containers/IntroductionPage/Testimonials';
 import Newsletter from '../containers/IntroductionPage/Newsletter';
 import Footer from '../containers/IntroductionPage/Footer';
 
-// import '../intro.css'
-// import '../other.css'
+import '../intro.css'
+import '../other.css'
 
 
 function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden" id="introductionPage">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.6/tailwind.min.css"></link>
+      {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.6/tailwind.min.css"></link> */}
       <link rel="stylesheet" href="../other.css"></link>
       <link rel="stylesheet" href="../intro.css"></link>
       
